7fb7b2817c217830ac373d9ef46737f9f9365280
alexandra-sharrky <sharrky.mail@gmail.com>
2019-02-16 16:04:03 +0300

    solving 6th

---
+++ 6fromthelast.py
@@ -0,0 +1,16 @@
+#l=input('Enter the list')
+#l.reverse()
+#print(l)
+#print(lis[1])
+#lis1=lis
+#lis.reverse()
+#if lis1==lis
+#    print("Your list is a palindrome")
+#else
+#    print("Your list is not a palindrome")
+
+input_list = int(input("enter a sequence of integers"))
+new_list = copy(input_list)
+print(new_list)
+#oo.reverse()
+#print(o)
